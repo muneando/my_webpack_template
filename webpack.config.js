aa0d9ef7e401ad16f4466b771b17a63a932dcafe
Ando Munenori <ando@and-works.co.jp>
2019-03-20 19:08:58 +0900

    SASSをコンパイルできるようにする。

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,83 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const AutoPrefixer = require('autoprefixer');
 
-module.exports = {
-  mode: 'development',
-  
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 8086,
-    open: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
+module.exports = [
+  {
+    entry: {
+      app: [
+        './src/js/app.js',
+        './src/sass/style.scss',
+      ],
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'js/app.js',
+    },
+    devServer: {
+      contentBase: './dist',
+      watchContentBase: true,
+      port: 3000,
+      open: true,
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  }
-};
+          query: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+        {
+          test: /\.scss$/,
+          use: [{
+            loader: MiniCssExtractPlugin.loader,
+          }, {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: true,
+              importLoaders: 2,
+            },
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: [
+                AutoPrefixer(
+                  {
+                    browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
+                  },
+                ),
+              ],
+            },
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            }
+          }],
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [
+        new OptimizeCSSAssetsPlugin(),
+        new UglifyJsPlugin(),
+      ],
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'css/style.css',
+      }),
+    ],
+  },
+];
